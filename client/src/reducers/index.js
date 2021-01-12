735398c8799245f63f50eaf314df448a1010ee9e
nntucao <ngtu.caong@gmail.com>
2021-01-12 16:06:15 +0100

    removed tested webpack

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'; 
-//import TaskBoardReducer from './TaskBoardReducer'
-import TaskListReducer from './TaskListReducer'
-
-export default combineReducers({
-    //task_boards: TaskBoardReducer
-    my_task_lists: TaskListReducer
-});  
--- client/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'; 
-import ReduxPromise from 'redux-promise';
-import rootReducer from '../reducers'; 
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk'; 
-
-const store = createStore(rootReducer, applyMiddleware(thunk))
-
-export default store; 
