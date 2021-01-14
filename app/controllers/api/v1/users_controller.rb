c3dfde7ca66e7959163ab9309f7809f4aaf51136
nntucao <ngtu.caong@gmail.com>
2021-01-14 22:12:32 +0100

    fixed ffi gem version

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token, if: :json_request?
+    respond_to :json
+  
+    def index
+      @users = User.all
+      render json: @users     
+    end
+  
+    def show
+      @task_lists = TaskList.find(params[:id])
+      render json: @task_lists
+    end
+  
+    def create
+      @task_lists = TaskList.new(task_lists_params)
+  
+      if @task_lists.save
+        render json: @task_lists, status: :created
+      else
+        render json: @task_lists.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      @task_lists = TaskList.find(params[:id])
+      if @task_lists.update(task_lists_params)
+        render json: @task_lists
+      else
+        render json: @task_lists.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @task_lists = TaskList.find(params[:id])
+      @task_lists.destroy
+    end
+  
+    private
+  
+    def task_lists_params
+      params.require(:task_list).permit(:name, :is_archived, :board_id)
+    end
+  
+    def json_request?
+      request.format.json?
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
 
-  authenticated :user do
-    root 'pages#my_boards', as: :authenticated_root
-  end
+  # authenticated :user do
+  #   root 'pages#my_boards', as: :authenticated_root
+  # end
 
   root 'pages#home'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :boards, only: [:index, :show, :create, :update, :destroy]
+      resources :users, only: [:index, :show, :create, :update, :destroy]
       resources :task_lists, only: [:index, :show, :create, :update, :destroy] do
         resources :task_cards, only: [:index, :show, :create, :update, :destroy]
       end
